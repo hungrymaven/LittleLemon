480c2df900e599e2413de489b7980d50c0c9ec8d
hungrymaven <89184598+hungrymaven@users.noreply.github.com>
2023-05-15 16:05:49 +0200

    created "newapp" app

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('littlelemonapp.urls')),
+    path('newdemo/', include('newapp.urls')),
 ]
