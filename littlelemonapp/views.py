c6b2cf9e1d2d615f834186bd49f260a56df3a33e
hungrymaven <89184598+hungrymaven@users.noreply.github.com>
2023-05-15 09:25:42 +0200

    mapping urls with params
    exercise: mapping urls with params

--- littlelemonapp/views.py
+++ littlelemonapp/views.py
@@ -2,5 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, world. This is the index of the LittleLemon App.")
+# def index(request):
+#     return HttpResponse("Hello, world. This is the index of the LittleLemon App.")
+
+
+def index(request): 
+    path = request.path 
+    method = request.method 
+    content=''' 
+<center><h2>Testing Django Request Response Objects</h2> 
+<p>Request path : " {}</p> 
+<p>Request Method :{}</p></center> 
+'''.format(path, method) 
+    return HttpResponse(content) 
+
+
+def drinks(request, drink_name):
+    drink = {
+        'mocha': 'type of coffee',
+        'tea': 'type of beverage',
+        'lemonade': 'type of refreshment',
+    }
+
+    choice_of_drink = drink.get(drink_name, 'Drink not available')
+
+    return HttpResponse(f"<h2>{drink_name}</h2>{choice_of_drink}")
